88507b4a7d26f81c3966474c069a8126602ecca4
Hermesis <hermesiss@gmail.com>
2021-11-15 09:33:43 +0400

    Small changes

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('_helpers/jwt');
 const errorHandler = require('_helpers/error-handler');
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -20,7 +23,8 @@ app.use('/users', require('./users/users.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
-const server = app.listen(port, function () {
+const port = process.env.NODE_ENV === 'production' ? process.env.PORT : 8080;
+const ip = process.env.NODE_ENV === 'production' ? '0.0.0.0' : process.env.IP;
+const server = app.listen(port, ip,function () {
     console.log('Server listening on port ' + port);
 });
